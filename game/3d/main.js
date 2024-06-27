8de34e14507c66fb66156c64f38347d353f5f32f
admansar <115159995+admansar@users.noreply.github.com>
2024-06-27 15:56:04 +0000

    3d_table

--- game/3d/main.js
+++ game/3d/main.js
@@ -5,11 +5,16 @@ const height = window.innerHeight
 
 
 // Cube details
-const cube_dimensions = { x: 2, y: 2, z: 2 }
-const cube_position = { x: 0, y: 0, z: 0 }
+const table_dimensions = { x: 0.06, y: 4.29, z: 6.76 }
+const table_position = { x: 0, y: 0, z: 2 }
+const table_rotation = { x: 0.3, y: 0, z: 1.57 }
+
+const net_dimensions = { x: 0.26, y: 4.29, z: 0.06 }
+const net_position = { x: 0, y: 0.18, z: 2 }
+const net_rotation = { x: 0.3, y: 0, z: 1.57 }
 
 // light details
-const light_position = { x: 2.5, y: 2, z: 2 }
+const light_position = { x: 2.5, y: 2, z: -0.3 }
 const light_color = 0xffffff
 const light_intensity = 0.3
 
@@ -34,7 +39,7 @@ const point_light_position = { x: 10, y: 5, z: 5 }
 
 
 
-const scene_color = '#000000'
+const scene_color = 0x000000
 
 
 function init_scene(scene_color)
@@ -46,16 +51,31 @@ function init_scene(scene_color)
 }
 
 // Creating a cube
-function create_cube (cube_dimensions, cube_position, cube_color)
+function create_cube (cube_dimensions, cube_position, cube_rotation, cube_color)
 {
     const geometry = new THREE.BoxGeometry(cube_dimensions.x, cube_dimensions.y, cube_dimensions.z)
     const material = new THREE.MeshStandardMaterial({ color: cube_color })
     const cube = new THREE.Mesh(geometry, material)
     cube.position.set(cube_position.x, cube_position.y, cube_position.z)
+    cube.rotation.set(cube_rotation.x, cube_rotation.y, cube_rotation.z)
     cube.castShadow = true
     cube.receiveShadow = true
     scene.add(cube)
     return cube
+}
+
+function create_net(net_dimensions, net_position, net_rotation, net_color)
+{
+    const geometry = new THREE.BoxGeometry(net_dimensions.x, net_dimensions.y, net_dimensions.z)
+    const material = new THREE.MeshStandardMaterial({ color: net_color/*, opacity: 0.5, side: THREE.DoubleSide*/})
+    const net = new THREE.Mesh(geometry, material)
+    net.position.set(net_position.x, net_position.y, net_position.z)
+    net.rotation.set(net_rotation.x, net_rotation.y, net_rotation.z)
+    net.castShadow = true
+    net.receiveShadow = true
+    scene.add(net)
+    return net
+
 }
 
 function create_sphere(sphere_dimensions, sphere_position, sphere_color)
@@ -125,6 +145,40 @@ function create_renderer(width, height)
     return renderer
 }
 
+function create_table_cadre (cube_dimensions, cube_position, cube_rotation, cube_color)
+{
+    const middle_line_dimensions = { x: cube_dimensions.x, y: cube_dimensions.y  / 100, z: cube_dimensions.z }
+    const middle_line_position = { x: cube_position.x, y: cube_position.y + 1e-5, z: cube_position.z }
+
+    const middle_line  = create_cube(middle_line_dimensions, middle_line_position, cube_rotation, cube_color)
+    scene.add(middle_line)
+
+    const front_line_dimensions = { x: cube_dimensions.x, y: cube_dimensions.y + 0.03, z: cube_dimensions.z / 100 }
+    const front_line_position = { x: cube_position.x , y: cube_position.y - 1.0, z: cube_position.z + table_dimensions.z / 2 - 0.11}
+
+    const front_line  = create_cube(front_line_dimensions, front_line_position, cube_rotation, cube_color)
+    scene.add(front_line)
+
+
+    const left_side_line_dimensions = { x: cube_dimensions.x , y: cube_dimensions.y  / 100, z: cube_dimensions.z }
+    const left_side_line_position = { x: cube_position.x - table_dimensions.y / 2 - 0.007, y: cube_position.y + 1e-5, z: cube_position.z + 0.001 }
+
+    const left_side_line  = create_cube(left_side_line_dimensions, left_side_line_position, cube_rotation, cube_color)
+    scene.add(left_side_line)
+
+    const right_side_line_dimensions = { x: cube_dimensions.x , y: cube_dimensions.y  / 100, z: cube_dimensions.z }
+    const right_side_line_position = { x: cube_position.x + table_dimensions.y / 2, y: cube_position.y + 1e-5, z: cube_position.z + 0.001 }
+
+    const right_side_line  = create_cube(right_side_line_dimensions, right_side_line_position, cube_rotation, cube_color)
+    scene.add(right_side_line)
+
+    const back_line_dimensions = { x: cube_dimensions.x, y: cube_dimensions.y + 0.03, z: cube_dimensions.z / 100 }
+    const back_line_position = { x: cube_position.x , y: cube_position.y + 1.01, z: cube_position.z - table_dimensions.z / 2 + 0.11}
+
+    const back_line  = create_cube(back_line_dimensions, back_line_position, cube_rotation, cube_color)
+    scene.add(back_line)
+
+}
 
 // init Scene
 const scene = init_scene(scene_color)
@@ -132,19 +186,26 @@ const camera = camera_init(camera_position, camera_fov, camera_aspect, camera_ne
 const renderer = create_renderer(width, height)
 const light = create_light(light_position, light_color, light_intensity, point_light_position)
 // const helper = light_helper(light)
-const plane = create_plane(plane_position, plane_color, plane_dimensions)
-// const cube = create_cube(cube_dimensions, cube_position, 0x87ce00)
-const sphere = create_sphere(sphere_dimensions, sphere_position, '#00ffff')
-
-
+// const plane = create_plane(plane_position, plane_color, plane_dimensions)
+const table = create_cube(table_dimensions, table_position, table_rotation,  0x51E338)
+const table_cadre = create_table_cadre(table_dimensions, table_position, table_rotation,  0xffffff)
+const net = create_net(net_dimensions, net_position, net_rotation,  0xffffff) // dak l3iba li bkhit f lwest d table
+// const sphere = create_sphere(sphere_dimensions, sphere_position, '#00ffff')
 
 // Rendering the scene
-function animate() {
+function animate()
+{
     requestAnimationFrame(animate)
     // cube.rotation.x += 0.005
     // cube.rotation.y += 0.01
     // cube.rotation.z += 0.01
-    sphere.position.y += Math.sin(Date.now() * 0.002) * 0.01
+    // console.log (`${cube.rotation.x}, ${cube.rotation.y}, ${cube.rotation.z}`)
+    // sphere.position.y += Math.sin(Date.now() * 0.002) * 0.02
+    // if (sphere.position.y + sphere_dimensions.radius < 0)
+    // {
+    //     sphere.position.y = 0
+    // }
+    // light.point_light_position.z = Math.sin(Date.now() * 0.01) * 5
     renderer.render(scene, camera)
 }
 
