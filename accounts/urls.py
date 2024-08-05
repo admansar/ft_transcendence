33df9b419d8d4efa64d0f3f667c15121ff6e62bc
Salim <salimiom@gmail.com>
2024-08-05 20:21:00 +0100

    refactor django project structure

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     # path('management',views.userManagement,name='management')
-    path('', views.userManagement, name='userManagement'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
 
 #http://127.0.0.1:8000/user/managenet
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 #i create first page
-def userManagement(request):
+def register(request):
     x = {'name':'ali'}
-    return render(request,'index_login.html',{'name' : 'Lorku'})
+    return render(request,'register.html',{'name' : 'Lorku'})
+
+def login(request):
+    x = {'name':'ali'}
+    return render(request,'login.html',{'name' : 'Lorku'})
 
 
 # Create your views here.
