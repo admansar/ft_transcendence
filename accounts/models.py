5b6068abca91cf4162876fc1e93184742bf97945
yassineberrim <yassineberrim99@gmail.com>
2024-08-06 21:07:51 +0100

    Here we go to create table model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(null=True, blank=True)  # Allow blank and null for optional field
+
+    def __str__(self):
+        return self.user.username
+
+class Action(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    flower = models.CharField(max_length=100)  # Assuming 'flower' is a text field
+    timestamp = models.DateTimeField(auto_now_add=True)  # To track when the action was performed
+
+    def __str__(self):
+        return f"Action by {self.user} with flower {self.flower} on {self.timestamp}"
--- ft_transcendence/urls.py
+++ ft_transcendence/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include('templates.urls')),
     path('accounts/', include('accounts.urls')),
 ]
