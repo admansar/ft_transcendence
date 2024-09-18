d87f5b20523a06ddb6bf89e129902a2196ca24ae
admansar <adnane.mansar@etu.uae.ac.ma>
2024-09-18 16:11:49 +0100

    1v1 remote works for same sizes

--- game/views.py
+++ game/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def game_2d(request):
     return render(request, 'game.html')
 
+
 @login_required
 def game_3d(request):
     return render(request, "game_3d.html")
+
 
 @login_required
 def select_game(request):
