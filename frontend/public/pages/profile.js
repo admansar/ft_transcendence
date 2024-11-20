23c0115332d0ff36d21bec88d9f96d0e56922427
ckannane <chafiqkannane@gmail.com>
2024-11-20 16:12:08 +0100

    ckannane: dashbord front-end salat

--- frontend/public/pages/profile.js
+++ frontend/public/pages/profile.js
@@ -130,9 +130,15 @@ class Profile extends HTMLElement {
                                 </div>
                             </div>
                             <div class="tools_profile">
+                             <span style="display:contents">
                             <span class="request_list pending_list" id="pending_list"></span>
                             <span class="request_list block_list" id="block_list"></span>
                             <span class="request_list share_profile" id="share_profil"></span>
+                            </span>
+                            <span style="display:none">
+                                <span class="request_list adding_friend" id="add_friend"></span>
+                                <span class="request_list block_user" id="block_that"></span>
+                            </span>
                             </div>
                         </div>
                     </div>
