42f17b1dac07c7e1cce0ebd7053a68424b5830a2
ckannane <chafiqkannane@gmail.com>
2024-11-22 00:54:02 +0100

    ckannane: homepage update

--- frontend/public/pages/homepage.js
+++ frontend/public/pages/homepage.js
@@ -3,11 +3,13 @@ import "../components/modal.js";
 import "../components/chat.js";
 import "../components/settings.js"
 import "../components/search.js";
+import "../components/menu.js"
 import { getUserDataByID } from "../services/utils.js";
 import { getToken } from "../services/utils.js";
 import app from "../components/state.js";
 
 export class HomePage extends HTMLElement {
+    
     constructor() {
         super();
     }
@@ -30,6 +32,7 @@ export class HomePage extends HTMLElement {
         // window.dispatchEvent(event);
         
         profile.style.backgroundImage = `url(${userData.avatar})`;
+        document.querySelector('.message').innerHTML = `${userData.username}`;
         profile.addEventListener('click', e => {
             e.preventDefault();
             app.router.findRoute(`/profile/${userData.username}`);
@@ -38,6 +41,8 @@ export class HomePage extends HTMLElement {
         const modalsComponent = document.createElement('app-modals');
         const ChatComponent = document.createElement('app-chat');
         const SettingsComponent = document.createElement('app-settings');
+        const menu = document.createElement('app-menu');
+        this.appendChild(menu);
         this.appendChild(SettingsComponent);
         this.appendChild(circlesComponent);
         this.appendChild(modalsComponent);
@@ -46,6 +51,7 @@ export class HomePage extends HTMLElement {
         const searchComponent = document.createElement('app-search');
         this.appendChild(searchComponent);
 
+        
     }
 }
 
