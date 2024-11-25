b303c280c8731e2ca9105f1419eb2e231c3c8d7c
admansar <115159995+admansar@users.noreply.github.com>
2024-11-25 13:58:24 +0000

    docker patches

--- auth_service/init.sh
+++ auth_service/init.sh
@@ -1,8 +1,9 @@
-python3 manage.py populate_users
+python helpers.py
 python3 manage.py makemigrations authentication_service
 python3 manage.py migrate authentication_service
 python3 manage.py makemigrations friends
 python3 manage.py migrate friends
 python3 manage.py makemigrations chat
 python3 manage.py migrate chat
+python3 manage.py populate_users
 exec "$@"
