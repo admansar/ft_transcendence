bffa0f9d4a79b66b50cf17410b50c7d67faee88e
admansar <adnane.mansar@etu.uae.ac.ma>
2024-11-25 20:31:47 +0100

    fixing make

--- backend/python/init.sh
+++ backend/python/init.sh
@@ -7,6 +7,6 @@ python manage.py migrate game
 python manage.py makemigrations tournament
 python manage.py migrate tournament
 
-python manage.py populate_users
+#python manage.py populate_users
 
 exec python manage.py runserver '0.0.0.0:8000'
