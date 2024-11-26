4068c2f7faf78e80455bbbf7cc3b52cef37994ca
Ayoub Bouram <abouram@c1r6p3.1337.ma>
2024-11-26 04:26:35 +0100

    rank

--- backend/transcendence/friends_game/views.py
+++ backend/transcendence/friends_game/views.py
@@ -5,10 +5,45 @@ from rest_framework.response import Response
 import requests
 
 class RankUser(APIView):
+    username = None
+    win = 0
+    loss = 0
     def get(self, request):
+        result = []
         _user = get_all_users()
-        user = _user[0]["username"]
-        response = requests.get(f'http://localhost:8000/api/game/get-games{user}/')
-        return Response(response)
-
+        for key in _user:
+            user = key["username"]
+            response = requests.post('http://localhost:8000/api/game/get-games',
+                json = {"username" : user},
+                headers = {"Content-Type": "application/json"},)
+            data = response.json()
+            if data['message'] == "No games found":
+               result.append({
+                    "username" : user,
+                    "win" : 0,
+                    "loss" : 0,
+               })
+            else:
+                for key in data['games']:
+                    usera = key["player_a_id"]
+                    userb = key['player_b_id']
+                    if key["player_a"] == user:
+                        if (usera > userb):
+                            self.win += 1
+                        else:
+                            self.loss += 1
+                    if key["player_b"] == user:
+                        if (userb > usera):
+                            self.win += 1
+                        else:
+                            self.loss += 1
+                temp = {
+                    "username" : user,
+                    "win" : self.win,
+                    "loss" : self.loss,
+                }
+                result.append(temp)
+            self.loss = 0
+            self.win = 0
+        return  Response(result)
 # Create your views here.
